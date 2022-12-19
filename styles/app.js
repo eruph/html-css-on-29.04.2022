05363eabca6a991354ebc3de8a1d21fdc25cc2bc
eruph <88218252+eruph@users.noreply.github.com>
2022-12-20 00:39:57 +0300

    Add files via upload

---
+++ styles/app.js
@@ -0,0 +1,39 @@
+// const age = 18
+// const firstName = 'Никита'
+// const lastName = 'Копылов'
+// console.log("Моё имя: " + firstName + ' ' + lastName + ', а мой возраст: ' + age)
+// console.log(age.toString())
+// function calculateAge(year){
+//     return 2022 - year
+// }
+// function logInfoAbout(name, year){
+//     const age = calculateAge(year)
+//     age >= 0 ? console.log('Человек по имени ' + name + ' сейчас имеет возраст ' + age) :  console.log('Такого человека не существует/не был рождён')
+// }
+// logInfoAbout('Никита', 2022)
+// const cars = ['Мазда', 'Мерседес','Форд']
+// cars[0] = 'Ауди ку пят'
+// cars[cars.length]= 'mazda'
+// console.log(cars)
+// const cars = ['Мазда', 'Мерседес','Форд']
+// for(let i = 0; i < cars.length; i++){
+//     const car = cars[i]
+//     console.log(car)
+// }
+// for (let car of cars)[
+//     console.log(car)
+// ]
+// const person = {
+//     firstName: 'Nikita',
+//     lastName: 'Kopylov',
+//     year: 2004,
+//     languages: ['Ru', 'En', 'Fr', 'Ch'],
+//     hasWife: false,
+//     greet: function(){
+//         console.log('greet')
+//     }
+// }
+// console.log(person.firstName)
+// person.greet()
+const age = prompt("123")
+console.log(age)
